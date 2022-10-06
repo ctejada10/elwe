cda408455adcfb0047122679fa98d286c2a5c0db
Carlos E. Tejada <carlos@carlosetejada.com>
2022-10-06 21:37:59 +0200

    Added genre property

--- src/entry.py
+++ src/entry.py
@@ -1,9 +1,10 @@
 class Entry (object):
-  def __init__(self, artist, album, score) -> None:
+  def __init__(self, artist, album, score, genre) -> None:
     self.artist = artist
     self.album  = album
     self.score  = score
+    self.genre  = genre
 
   
   def __repr__(self) -> str:
-    return f'{self.artist} - {self.album}: {self.score}'
+    return f'{self.artist} - {self.album} - ({self.genre}): {self.score}'
