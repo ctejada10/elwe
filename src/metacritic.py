f90b44fa9d1170a558c062b5f8016e1bd23c1875
Carlos E. Tejada <carlos@carlosetejada.com>
2023-02-15 10:40:44 +0100

    Adding basic config for metacritic
    This is the basic config for album fetching on metacritic.

--- src/metacritic.py
+++ src/metacritic.py
@@ -24,13 +24,14 @@ class Metacritic (Scrapper):
       release_dates = soup.find_all('div', class_='clamp-details')
 
       for album, artist, score, date in zip(album_titles, artist_names, album_scores, release_dates):
-        t = album.find('h3').get_text()
-        a = artist.get_text().strip()[3:]
-        d = datetime.strptime(date.find('span').get_text(), '%B %d, %Y')
-        s = int(score.find('div', class_='metascore_w').get_text())
-        g = self._get_genre(album)
-
-        print(t)
+        try:
+          t = album.find('h3').get_text()
+          a = artist.get_text().strip()[3:]
+          d = datetime.strptime(date.find('span').get_text(), '%B %d, %Y')
+          s = int(score.find('div', class_='metascore_w').get_text())
+          g = self._get_genre(album)
+        except:
+          continue
 
         if (t == self.config['last_import_title']):
           last_import = False
@@ -48,12 +49,13 @@ class Metacritic (Scrapper):
 
 
   def _get_genre(self, album_tag):
-    href       = album_tag.attrs['href']
+    href       = album_tag.attrs['href'][1:]
     url        = self.config['base_url'] + href
     album_page = self._get_contents(url)
 
-    genre = album_page.find('li', class_='product_genre').find('span', class_='data').get_text()
-    return genre
+    if len(album_page):
+      genre = album_page.find('li', class_='product_genre').find('span', class_='data').get_text()
+      return genre
 
 if __name__ == '__main__':
   m = Metacritic('../config/metacritic.json')
