29b21156540285b563781bef9d9e989e2f2ce8c4
Carlos E. Tejada <carlos@carlosetejada.com>
2023-02-27 18:59:11 +0000

    Clean up of outputs for cron schedule

--- src/elwe.py
+++ src/elwe.py
@@ -12,13 +12,13 @@ def main(config_folder_path):
   unfiltered_releases = m.get_entries()
   filtered_releases   = filter_releases(unfiltered_releases, m.config)
 
-  for release in (pbar:= tqdm(filtered_releases)):
+  for release in filtered_releases:
     album_id = am.search_album(release.album.translate(str.maketrans('', '', string.punctuation)), 
                                release.artist.translate(str.maketrans('', '', string.punctuation)))
     if album_id is not None:
       am.add_album_to_library(album_id)
   
-  status = f'Added {len(filtered_releases)} albums to the library.'
+  status = f'Added {len(filtered_releases)} albums to the library' if len(filtered_releases) > 0 else 'No new albums added'
   r = requests.post('https://api.pushcut.io/HwnjkzpA86ntxL6NxPnS6/notifications/New%20albums%20added', 
                     json={'text': status})
 
@@ -34,5 +34,5 @@ def filter_releases(entries, config):
 
 
 if __name__ == "__main__":
-  logging.basicConfig(level=logging.WARN)
+  logging.basicConfig(filename='/var/log/elwe.log', level=logging.WARN)
   run(main)
